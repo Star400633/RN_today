f08431c2daf0ef250ab638bd8eb86baf2a447a07
baixiangguo <baixiangguo@2dfire.com>
2019-04-25 11:23:31 +0800

    配置相关

--- js/api/config.js
+++ js/api/config.js
@@ -1,7 +1,7 @@
 export default {
     GET_TODAY_DATA: {
         name: '获取今日信息', // 获取今日信息
-        path: 'http://localhost:1220/history/v1/list',
+        path: 'http://10.1.131.8:1220/history/v1/list',
         type: 'GET',
         showloading: true,
     }
